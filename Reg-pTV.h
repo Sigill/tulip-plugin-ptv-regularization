9dc0243be552632857009b80e031dcd834fbe443
Cyrille Faucheux <cyrille.faucheux@gmail.com>
2013-01-24 17:55:19 +0100

    First version, with support for isotropic, anisotropic, and both.

---
+++ Reg-pTV.h
@@ -0,0 +1,27 @@
+#ifndef _CV_TA_H
+#define _CV_TA_H
+
+#include <tulip/TulipPlugin.h>
+#include <utility>
+
+class Reg_pTV:public tlp::Algorithm {
+private:
+	unsigned int iter_max;
+	tlp::DoubleProperty *f0, *wl, *fn_result;
+	bool is_isotropic, is_anisotropic;
+	double p, q, epsilon;
+
+	unsigned int export_interval;
+	std::string export_directory;
+
+public:
+	Reg_pTV(const tlp::AlgorithmContext& context);
+	~Reg_pTV() {}
+	bool run();
+	bool check(std::string &);
+
+private:
+	void exportGraph(const int i);
+};
+
+#endif
